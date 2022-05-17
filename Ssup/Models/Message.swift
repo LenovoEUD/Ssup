935e7dff22f65713bbbb30747d2255b1b2933e8a
Erman Ufuk Demirci <ermanufukdemirci@gmail.com>
2022-05-17 10:40:54 +0300

    Complete Ssup app

---
+++ Ssup/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Ssup
+//
+//  Created by Erman Ufuk Demirci on 5.05.2022.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
